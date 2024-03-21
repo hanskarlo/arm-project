923e8fb3dc1451b81b45c1bf8ac7da5e3a01cf61
hanskarlo <hansjarales@gmail.com>
2024-03-21 12:37:11 -0600

    move group interface class, updated arm msgs

--- zeroerr_move_group/include/zeroerr_move_group/zeroerr_move_group.h
+++ zeroerr_move_group/include/zeroerr_move_group/zeroerr_move_group.h
@@ -1,3 +1,5 @@
+#include <rclcpp/rclcpp.hpp>
+
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
@@ -12,5 +14,33 @@
 #include "zeroerr_msgs/msg/arm_joint_space.hpp"
 #include "zeroerr_msgs/msg/arm_point.hpp"
 
-zeroerr_msgs::msg::ArmJointSpace arm_joint_space_cmd_;
-zeroerr_msgs::msg::ArmPoint arm_point_cmd_;
+
+class ArmMoveGroup
+{
+    public:
+        ArmMoveGroup();
+        ~ArmMoveGroup();
+
+        rclcpp::Node::SharedPtr node_;
+
+    private:
+        const uint NUM_JOINTS = 6;
+        const float PI = 3.141592654;
+        const std::string LOGGER = "arm_move_group";
+        const std::string PLANNING_GROUP = "arm_group";
+        const std::string NODE_NAME = "arm_move_group";
+
+        rclcpp::Subscription<zeroerr_msgs::msg::ArmJointSpace>::SharedPtr arm_joint_space_sub_;
+        rclcpp::Subscription<zeroerr_msgs::msg::ArmPoint>::SharedPtr arm_point_sub_;
+        void arm_joint_space_cb_(zeroerr_msgs::msg::ArmJointSpace::SharedPtr goal_msg);
+        void arm_point_cb_(zeroerr_msgs::msg::ArmPoint::SharedPtr goal_msg);
+
+
+        zeroerr_msgs::msg::ArmJointSpace arm_joint_space_cmd_;
+        zeroerr_msgs::msg::ArmPoint arm_point_cmd_;
+        moveit::planning_interface::MoveGroupInterfacePtr move_group_;
+        moveit::planning_interface::MoveGroupInterface::Plan plan;
+        moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+        // moveit::core::JointModelGroup* joint_model_group_;
+
+};
---
+++ zeroerr_move_group/launch/move_group.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("zeroerr_arm", package_name="zeroerr_config").to_moveit_configs()
+
+    # MoveGroupInterface demo executable
+    move_group = Node(
+        # name="arm_move_group",
+        package="zeroerr_move_group",
+        executable="zeroerr_move_group",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {"use_sim_time": True}
+        ],
+    )
+
+    return LaunchDescription([move_group])
